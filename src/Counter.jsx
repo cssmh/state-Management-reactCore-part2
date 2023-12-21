d73f4e805ec9a2e636683384aad3192a46a0886a
momen210 <touristmomen@gmail.com>
2023-12-21 13:00:59 +0600

    some changes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Counters() {
-    const [count, setCount] = useState(0);
-    const handleCount = () => {
-        const newCount = count + 1;
-        setCount(newCount)
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+  const handleReduce = () => {
+    const reduceCount = count - 1;
+    if (count > 0) {
+      setCount(reduceCount);
+    } else {
+      alert("It's already 0 bro");
     }
-    const handleReduce = () => {
-        const reduceCount = count - 1;
-        if(count>0){
-            setCount(reduceCount)
-        }
-    }
+  };
 
-    return (
-        <div style={{border: '2px solid yellow', marginBottom: '10px', paddingBottom: '15px'}}>
-            <h3>Counter: {count}</h3>
-            <button onClick={handleCount}>Count</button>
-            <button onClick={handleReduce}>Reduce</button>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        border: "2px solid yellow",
+        marginBottom: "10px",
+        paddingBottom: "15px",
+      }}
+    >
+      <h3>Counter: {count}</h3>
+      <button className="gap" onClick={handleCount}>Count</button>
+      <button onClick={handleReduce}>Reduce</button>
+    </div>
+  );
 }
