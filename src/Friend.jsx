6d3e235588a2566f14b68dba7b6e3cf70ec34fec
momen210 <touristmomen@gmail.com>
2023-12-23 22:05:21 +0600

    new added

--- src/Friend.jsx
+++ src/Friend.jsx
@@ -1,9 +1,12 @@
 export default function Friend({getFriend}){
-    const {name, email} = getFriend;
+    const {name, email, phone, address} = getFriend;
+    console.log(getFriend)
     return (
-        <div>
+        <div className="box">
             <h4>Name: {name}</h4>
             <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
+            <p>Zip: {address.zipcode}</p>
         </div>
     )
 }
